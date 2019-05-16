2cd37593c52874da3d723bd9c8f5b1f086c43afb
이승철 <scin21c@gmail.com>
2019-05-16 14:12:56 +0900

    tutorial에 따라 SocialApplication 추가 및 @EnableOAuth2Sso 어노테이션 이동

---
+++ spring.io/Spring_Boot_and_OAuth2/ui/src/main/java/com/example/demo/SocialApplication.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SocialApplication {
+}
