57f172a22c8ee0ea74c94f1644b6e9ce3f4ea299
LeeSeungChul <scin21c@gmail.com>
2019-05-21 00:02:26 +0900

    WebSecurityConfigurerAdapter를 확장하여 Social Login 시 authentication 에 따른 uri 접근 제한을 처리한다.

--- spring.io/Spring_Boot_and_OAuth2/ui/src/main/java/com/example/demo/SocialApplication.java
+++ spring.io/Spring_Boot_and_OAuth2/ui/src/main/java/com/example/demo/SocialApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,18 @@ import java.security.Principal;
 @SpringBootApplication
 @EnableOAuth2Sso
 @RestController
-public class SocialApplication {
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+					.antMatchers("/", "/login**", "/webjars/**", "/error**")
+					.permitAll()
+				.anyRequest()
+					.authenticated();
+	}
 
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
